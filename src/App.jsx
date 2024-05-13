6fe81820a0d0fb878b9972559f3cefd39218b38d
sanketshivale <shivalesanket21@gmail.com>
2024-05-13 21:00:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='text-2xl text-green-500 font-bold'>Hello Vite + React + GraphQL!</h1>
+        
+      </div>
+       
+    </>
+  )
+}
+
+export default App
